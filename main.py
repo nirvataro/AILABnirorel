3225df63e6c810f3bd708e34a80201ff8221bc96
nirvataro <nvataro@gmail.com>
2021-03-20 19:09:36 +0200

    with cosmetics

--- main.py
+++ main.py
@@ -1,32 +1,27 @@
 import GeneticAlgorithm as GA
 import time
-import copy
+import random
 
 MAXITER = 2000
-POPSIZE = 2000
+POPSIZE = 20
 MAX_WEIGHT = 165
 ITEM_PRICE = [92, 57, 49, 68, 60, 43, 67, 84, 87, 72]
 SOLUTION = [1, 1, 1, 1, 0, 1, 0, 0, 0, 0]
 ITEM_WEIGHT = [23, 31, 29, 44, 53, 38, 63, 85, 89, 82]
+PENALTY = 10
 
 
-def generateBag(item_weight):
-    sum = 0
-    bag = [0 for i in range(len(item_weight))]
-    indices = list(range(len(item_weight)))
-    while sum < MAX_WEIGHT:
-        index = GA.random.choice(indices)
-        bag[index] = 1
-        sum += item_weight[index]
-        indices.remove(index)
-    bag[index] = 0
+def generateBag():
+    bag = [None for i in range(len(SOLUTION))]
+    for i in range(len(SOLUTION)):
+        bag[i] = random.choice([0, 1])
     return bag
 
 
 def init(item_weight):
     pop, buffer = [], []
     for i in range(POPSIZE):
-        ran_str = generateBag(item_weight)
+        ran_str = generateBag()
         pop.append(GA.Genetic(ran_str))
         buffer.append(GA.Genetic(ran_str))
 
@@ -35,10 +30,15 @@ def init(item_weight):
 
 class Knapsack:
     def calc_personal_fitness(self, bag):
-        fitness = MAX_WEIGHT
+        fitness = sum(ITEM_PRICE)
+        total_weight = 0
         for i in range(len(bag.str)):
-            if not bag.str[i]:
-                fitness += ITEM_PRICE[i] - ITEM_WEIGHT[i]
+            if bag.str[i]:
+                fitness -= ITEM_PRICE[i]
+                total_weight += ITEM_WEIGHT[i]
+        overweight = total_weight - MAX_WEIGHT
+        if overweight > 0:
+            fitness += overweight*PENALTY
         return fitness
 
 
@@ -61,7 +61,7 @@ def main():
 
         gen_arr = GA.birthday(gen_arr)
         # mate and swap between buffer and gen_arr
-        buffer, gen_arr = GA.mate(gen_arr, buffer, GA.UniCross(), GA.REGULAR())
+        buffer, gen_arr = GA.mate(gen_arr, buffer, GA.UniCross(), GA.REGULAR(), [0,1], tar_len=len(SOLUTION))
 
     print("Total time : {}\nTotal clock ticks : {}\nTotal iter:{}".format(time.time() - totaltimer, time.process_time() - totalticks, i+1))
 
